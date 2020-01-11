bee7e0333ecc7f8531574043bafba9c6d33bc6b9
锅某人 <596278576@qq.com>
2020-01-11 13:56:01 +0800

    行 封装完毕

---
+++ src/components/myrow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="myrow">
+      <span>{{left}}</span>
+      <span>{{right}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['left', 'right']
+}
+</script>
+
+<style lang='less' scoped>
+.myrow{
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+}
+</style>
--- src/views/edit_profile.vue
+++ src/views/edit_profile.vue
@@ -4,14 +4,16 @@
         <myheader title="编辑信息">
             <van-icon name="arrow-left" slot="left" @click="$router.back()" />
         </myheader>
+        <myrow left='用户名' right='123'></myrow>
     </div>
 </template>
 
 <script>
 import myheader from '@/components/header.vue'
+import myrow from '@/components/myrow.vue'
 export default {
   components: {
-    myheader
+    myheader, myrow
   }
 };
 </script>
