f356d9546b3969ea8f8b7553024587170aa4d011
锅某人 <596278576@qq.com>
2020-01-11 15:28:52 +0800

    修改密码功能实现

--- src/views/edit_profile.vue
+++ src/views/edit_profile.vue
@@ -14,11 +14,16 @@
     <van-uploader :after-read="afterRead" id="img" />
     <myrow left="昵称" :right="user.nickname" @click="show1=!show1"></myrow>
     <!-- 修改昵称弹框 -->
-    <van-dialog v-model="show1" title="修改昵称" show-cancel-button @confirm='upnickname'>
-        <van-field :value="user.nickname" placeholder="请输入昵称" ref='nick'/>
+    <van-dialog v-model="show1" title="修改昵称" show-cancel-button @confirm="upnickname">
+      <van-field :value="user.nickname" placeholder="请输入昵称" ref="nick" />
     </van-dialog>
 
-    <myrow left="密码" :right="user.password"></myrow>
+    <myrow left="密码" :right="user.password" @click="show2=!show2"></myrow>
+    <!-- 修改密码 -->
+    <van-dialog v-model="show2" title="修改密码" show-cancel-button @confirm="uppwd" :before-close='beforeclose'>
+      <van-field  placeholder="请输入原密码" ref="oldpwd" />
+      <van-field  placeholder="请输入新密码" ref="newpwd" />
+    </van-dialog>
 
     <myrow left="性别" :right="user.gender===0?'女':'男'"></myrow>
 
@@ -35,7 +40,8 @@ export default {
   data() {
     return {
       user: {},
-      show1: false
+      show1: false,
+      show2: false
     };
   },
   components: {
@@ -62,13 +68,42 @@ export default {
       }
     },
     async upnickname() {
-    //   console.log(this.$refs.nick.$refs.input.value);
-      let res = await userupdate(this.user.id, { nickname: this.$refs.nick.$refs.input.value })
+      //   console.log(this.$refs.nick.$refs.input.value);
+      let res = await userupdate(this.user.id, {
+        nickname: this.$refs.nick.$refs.input.value
+      });
       console.log(res);
-      this.$toast(res.data.message)
+      this.$toast(res.data.message);
       if (res.data.message === '修改成功') {
-        this.user.nickname = res.data.data.nickname
+        this.user.nickname = res.data.data.nickname;
       }
+    },
+    async beforeclose(action, done) {
+      let old = this.$refs.oldpwd.$refs.input.value
+      let n = this.$refs.newpwd.$refs.input.value
+      if (action === 'confirm') {
+        if (old !== this.user.password) {
+          this.$toast.fail('原密码错误')
+          this.$refs.oldpwd.$refs.input.focus()
+          done(false)
+        } else if (!/\w{3,16}/.test(n)) {
+          this.$toast.fail('新密码格式正确')
+          done(false)
+        } else {
+          let res = await userupdate(this.user.id, { password: n })
+          //   console.log(res);
+          this.$toast(res.data.message)
+          if (res.data.message === '修改成功') {
+            this.$router.push({ name: 'login' })
+          }
+          done()
+        }
+      } else {
+        done()
+      }
+    },
+    async uppwd() {
+
     }
   },
   async mounted() {
